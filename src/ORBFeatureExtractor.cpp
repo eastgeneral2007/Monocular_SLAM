da5d5a64a263649c7e3549e252b9dfecd7ec7200
JimXing <xyf.jim@gmail.com>
2016-12-04 01:38:28 -0500

    add scale recording in ORBFeatureExtractor

--- src/ORBFeatureExtractor.cpp
+++ src/ORBFeatureExtractor.cpp
@@ -20,6 +20,7 @@ void ORBFeatureExtractor::process(DataManager& data, int frameIdx) {
 	frame.features.descriptors = descriptor;
 	for (int i=0; i<keypoints.size(); i++) {
 		frame.features.positions.push_back(keypoints[i].pt);
+		frame.features.scales.push_back((double)(keypoints[i].size));
 	}
 	frame.features.mapPointsIndices.resize(keypoints.size(), -1);
 
